e6a89c43fd72718870c8ee054a7a8ea3bb05a805
Pedro Sandes <pedroalves@pecege.com>
2022-02-17 21:36:16 -0300

    feat: Utilizar o globalStyles e o DefaultTheme

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { AppRoutes } from './router'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/defaultTheme'
+import { GlobalStyle } from './styles/globalStyles'
 import './styles/index.css'
-import { Home } from './pages'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <ThemeProvider theme={defaultTheme}>
+      <AppRoutes />
+      <GlobalStyle />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
